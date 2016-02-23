09eca2131d28c105772de8a356a4de8dc7dbae3a
Michael Ellery <mellery@gmail.com>
2016-02-23 09:32:09 -0500

    adding eclipse project which implements LED block cipher ontop of blinky project

---
+++ blinky/Debug/src/led-bytes.d
@@ -0,0 +1,6 @@
+src/led-bytes.o: ../src/led-bytes.cpp ../include/led-bytes.h \
+ ../system/include/diag/Trace.h
+
+../include/led-bytes.h:
+
+../system/include/diag/Trace.h:
---
+++ blinky/Debug/src/subdir.mk
@@ -0,0 +1,60 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/BlinkLed.cpp \
+../src/Timer.cpp \
+../src/led-bytes.cpp \
+../src/main.cpp 
+
+C_SRCS += \
+../src/_initialize_hardware.c \
+../src/_write.c \
+../src/stm32f4xx_hal_msp.c 
+
+OBJS += \
+./src/BlinkLed.o \
+./src/Timer.o \
+./src/_initialize_hardware.o \
+./src/_write.o \
+./src/led-bytes.o \
+./src/main.o \
+./src/stm32f4xx_hal_msp.o 
+
+C_DEPS += \
+./src/_initialize_hardware.d \
+./src/_write.d \
+./src/stm32f4xx_hal_msp.d 
+
+CPP_DEPS += \
+./src/BlinkLed.d \
+./src/Timer.d \
+./src/led-bytes.d \
+./src/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C++ Compiler'
+	arm-none-eabi-g++ -mcpu=cortex-m4 -mthumb -mfloat-abi=soft -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-move-loop-invariants -Wall -Wextra  -g3 -DDEBUG -DUSE_FULL_ASSERT -DOS_USE_SEMIHOSTING -DTRACE -DOS_USE_TRACE_SEMIHOSTING_DEBUG -DSTM32F407xx -DUSE_HAL_DRIVER -DHSE_VALUE=8000000 -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4-hal" -std=gnu++11 -fabi-version=0 -fno-exceptions -fno-rtti -fno-use-cxa-atexit -fno-threadsafe-statics -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=soft -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-move-loop-invariants -Wall -Wextra  -g3 -DDEBUG -DUSE_FULL_ASSERT -DOS_USE_SEMIHOSTING -DTRACE -DOS_USE_TRACE_SEMIHOSTING_DEBUG -DSTM32F407xx -DUSE_HAL_DRIVER -DHSE_VALUE=8000000 -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4-hal" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/stm32f4xx_hal_msp.o: ../src/stm32f4xx_hal_msp.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=soft -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-move-loop-invariants -Wall -Wextra  -g3 -DDEBUG -DUSE_FULL_ASSERT -DOS_USE_SEMIHOSTING -DTRACE -DOS_USE_TRACE_SEMIHOSTING_DEBUG -DSTM32F407xx -DUSE_HAL_DRIVER -DHSE_VALUE=8000000 -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4-hal" -std=gnu11 -Wno-missing-prototypes -Wno-missing-declarations -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/stm32f4xx_hal_msp.d" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
